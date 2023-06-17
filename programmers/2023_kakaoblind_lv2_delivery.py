073806c74ab0c37197853a16050cadd5efa75db6
gamju <atlantice123@gmail.com>
2023-06-18 04:26:35 +0900

    2023kakao_blindv2_correct

--- programmers/2023_kakaoblind_lv2_delivery.py
+++ programmers/2023_kakaoblind_lv2_delivery.py
@@ -1,43 +1,56 @@
 def solution(cap, n, deliveries, pickups):
     distance = 0
-    while(deliveries.__len__() or pickups.__len__()):
-        deliver_sum = 0
-        for i in deliveries:
-            deliver_sum += i
+    
+    deliver_sum = 0
+    deliveriey_distance = []
+    pickups_distance = []
+    for i in range(deliveries.__len__()):
+        if deliveries[i] != 0:
+            deliveriey_distance.append(i)
+            deliver_sum += deliveries[i]
+    for i in range(pickups.__len__()):
+        if pickups[i] != 0:
+            pickups_distance.append(i)
+    
+    print(deliveries.__len__(), pickups.__len__(), deliver_sum)
+    while(deliveriey_distance or pickups_distance):
+        # deliveries.pop()
+        
         cur_cap = min(cap, deliver_sum)
-        deliver_distance = deliveries.__len__()
-        pickup_distance = pickups.__len__()
-        cur_distance = max(deliver_distance, pickup_distance)
+        try:
+            delivery_dist = deliveriey_distance[-1] + 1
+        except:
+            delivery_dist = 0
+        
+        try:
+            pickups_dist = pickups_distance[-1] + 1
+        except:
+            pickups_dist = 0
+            
+        cur_distance = max(delivery_dist, pickups_dist)
         distance += cur_distance*2
-        for deliveries_idx in range(deliveries.__len__())[::-1]:
-            if cur_cap > deliveries[deliveries_idx]:
-                cur_cap = cur_cap - deliveries[deliveries_idx]
-                deliveries[deliveries_idx] = 0
+        while(cur_cap and deliveriey_distance):
+            if cur_cap >= deliveries[deliveriey_distance[-1]]:
+                deliver_sum = deliver_sum - deliveries[deliveriey_distance[-1]]
+                cur_cap = cur_cap - deliveries[deliveriey_distance[-1]]
+                deliveriey_distance.pop()
             else:
-                deliveries[deliveries_idx] = deliveries[deliveries_idx] - cur_cap
+                deliver_sum = deliver_sum - cur_cap
+                deliveries[deliveriey_distance[-1]] = deliveries[deliveriey_distance[-1]] - cur_cap
                 cur_cap = 0
-                break
         
-        for pickup_idx in range(pickups.__len__())[::-1]:
-            if cur_cap + pickups[pickup_idx] < cap:
-                cur_cap = cur_cap + pickups[pickup_idx] 
-                pickups[pickup_idx] = 0
+        while((cur_cap != cap) and pickups_distance):
+            if cur_cap + pickups[pickups_distance[-1]] <= cap:
+                cur_cap = cur_cap + pickups[pickups_distance[-1]]
+                pickups_distance.pop()
             else:
-                pickups[pickup_idx] = pickups[pickup_idx] - (cap - cur_cap)
-                break
-        # print(distance, deliver_distance, pickup_distance)
-            
-        while(deliver_distance > 0):
-            deliver_distance -= 1
-            if deliveries[-1] == 0:
-                deliveries = deliveries[:-1]
-        while(pickup_distance > 0):
-            pickup_distance -= 1
-            if pickups[-1] == 0:
-                pickups = pickups[:-1]
-                # pickups.pop()
+                pickups[pickups_distance[-1]] = pickups[pickups_distance[-1]] - (cap - cur_cap)
+                cur_cap = cap
         
-            
+        # while(deliveries[-1] == 0):
+        #     deliveries.pop()
+        # while(pickups[-1] == 0):
+        #     pickups.pop()
         
     answer = distance
     return answer
